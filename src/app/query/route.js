4d8e85e716283d28e56499a4ee668328d3b27553
Stanley Zhao <stanleyhzhao@gmail.com>
2023-09-17 01:47:11 -0400

    Update route.js

--- src/app/query/route.js
+++ src/app/query/route.js
@@ -7,13 +7,15 @@ export async function POST(request) {
 
   if (type === "filter") {
     const res = await fetch("http://localhost:5000/filter", {
+      method: "POST",
       body: JSON.stringify({ query }),
     });
     const data = await res.json();
     return NextResponse.json({ data: data.images, type: "filter" });
   } else if (type === "qna") {
     const res = await fetch("http://localhost:5000/qna", {
-      body: JSON.stringify({ filename: req.selectedImage, query }),
+      method: "POST",
+      body: JSON.stringify({ filename: req.selectedImage, prompt: query }),
     });
     const data = await res.json();
     return NextResponse.json({ data: data.response, type: "qna" });
