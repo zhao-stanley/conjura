f73b86ece60ce8eabdddc812ad7ebff30207c3a0
Stanley Zhao <stanleyhzhao@gmail.com>
2023-09-17 12:16:16 -0400

    Fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Conjura",
-  description: "Conjura",
+  title: "Conjure",
+  description: "Conjure",
 };
 
 export default function RootLayout({ children }) {
--- src/app/query/route.js
+++ src/app/query/route.js
@@ -6,14 +6,14 @@ export async function POST(request) {
   const query = req.query;
 
   if (type === "filter") {
-    const res = await fetch("http://localhost:5000/filter", {
+    const res = await fetch("http://127.0.0.1:5000/filter", {
       method: "POST",
       body: JSON.stringify({ query }),
     });
     const data = await res.json();
     return NextResponse.json({ data: data.images, type: "filter" });
   } else if (type === "qna") {
-    const res = await fetch("http://localhost:5000/qna", {
+    const res = await fetch("http://127.0.0.1:5000/qna", {
       method: "POST",
       body: JSON.stringify({ filename: req.selectedImage, prompt: query }),
     });
