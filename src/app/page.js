f6f2c6c7cf85471eecba054ceb684dfa563ca839
Stanley Zhao <stanleyhzhao@gmail.com>
2023-09-17 01:26:38 -0400

    metadata

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Interface from "@/components/Interface";
 export default function Home() {
   return (
     <main className="w-full h-full min-h-screen flex flex-row bg-white">
-      <Interface/>
+      <Interface />
     </main>
   );
 }
